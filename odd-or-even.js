c4b07b9c08e36456f4149dbc64a7e0b0bde29e44
jfranklin12 <j.franks725@gmail.com>
2023-01-18 15:37:35 -0800

    odd or even algo complete

---
+++ odd-or-even.js
@@ -0,0 +1,9 @@
+// Write code to return the string "odd" if `num` is an odd number
+// and return the string "even" if `num` is an even number
+
+var oddOrEven = function(num) {
+    if(Number.isInteger(num%2)){
+    return "even";
+    }
+    return "odd";
+};
