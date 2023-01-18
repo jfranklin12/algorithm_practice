278b4e513aac96be8d8064209cc0ca705775fe3e
jfranklin12 <j.franks725@gmail.com>
2023-01-18 15:36:40 -0800

    arithmetic functions complete

---
+++ arithmetic.js
@@ -0,0 +1,21 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+};
+
+var subtract = function(num1, num2) {
+    let dif = num1 - num2;
+    return dif;
+};
+
+var multiply = function(num1, num2) {
+    let product = num1 * num2;
+    return product;
+};
+
+var divide = function(num1, num2) {
+    let quo = num1 / num2;
+    return quo;
+};
