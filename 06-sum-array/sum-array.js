21965e0f81f6debcc20e78ecf03d3c44a01a9684
jfranklin12 <j.franks725@gmail.com>
2023-01-19 14:00:33 -0800

    sum array solved

--- 06-sum-array/sum-array.js
+++ 06-sum-array/sum-array.js
@@ -1,5 +1,16 @@
 // Write code to add all the numbers in `arr` and return the total
 
 var sumArray = function(arr) {
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    console.log(sum);
+
+    return sum;
 };
+
+
+sumArray([3, 1, 5, 19])
 
