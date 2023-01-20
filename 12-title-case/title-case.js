89566e068593c06ef0662808ac8b8bf2eddd1d38
jfranklin12 <j.franks725@gmail.com>
2023-01-20 14:02:50 -0800

    title case solved

--- 12-title-case/title-case.js
+++ 12-title-case/title-case.js
@@ -1,18 +1,20 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
 var titleCase = function(str) {
+    var upperCaseWord = [];
     
     var newStr = str.split(" ");
-        
+
     for(var i = 0; i < newStr.length; i++){
-        console.log(newStr.length);
-        var letters = newStr[i].toString().split("");
-        console.log(letters);
-        var firstLetter = letters[0].toString().toUpperCase();
-        console.log(firstLetter);
-        
+        var letters = newStr[i].split("");
+        // console.log(letters);
+        letters[0] = letters[0].toUpperCase();
+        // console.log(letters);
+        upperCaseWord.push(letters.join(""));
     }
+    // console.log(upperCaseWord.join(" "));
+    return upperCaseWord.join(" ");
     
 };
 
-titleCase("open the can")
+titleCase("the food is here")
