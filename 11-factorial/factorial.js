b23c99063e2e426a9585abd32894eead3165b263
jfranklin12 <j.franks725@gmail.com>
2023-01-20 12:29:59 -0800

    factorial solved

--- 11-factorial/factorial.js
+++ 11-factorial/factorial.js
@@ -1,3 +1,13 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+    let product = 1;
+    for(var i = 1; i <= num; i++){
+        product *= i
+
+    }
+    console.log(product)
+    return product;
+};
+
+factorial(10);
