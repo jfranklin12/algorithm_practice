58a6937c6564061e613518850aa037fb5c2f0f91
jfranklin12 <j.franks725@gmail.com>
2023-01-20 11:24:53 -0800

    is palidrome solved

--- 10-is-palindrome/is-palindrome.js
+++ 10-is-palindrome/is-palindrome.js
@@ -1,3 +1,12 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+    var newStr = "";
+    for(var i = str.length - 1; i >= 0; i--){
+        newStr += str[i];
+    } if(newStr === str) {
+        return true;
+    } else {
+        return false;
+    }
+};
