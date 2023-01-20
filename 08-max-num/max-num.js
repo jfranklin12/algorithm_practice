34a742832b7672f7335d748cb91dce0139bb5c29
jfranklin12 <j.franks725@gmail.com>
2023-01-19 16:20:46 -0800

    max num solved

--- 08-max-num/max-num.js
+++ 08-max-num/max-num.js
@@ -1,3 +1,15 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    var max = arr[0];
+    for(var i = 0; i < arr.length; i++){
+        var num = arr[i];
+        if(num > max){
+            max = num;
+        }
+    }
+    console.log(max);
+    return max;
+};
+
+maxNum([3, 1, 17, 5, 6]);
