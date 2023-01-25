aeb53421126e981272c9eae369456b97911cf136
jfranklin12 <j.franks725@gmail.com>
2023-01-25 10:46:06 -0800

    is unique solved

--- 17-is-unique/is-unique.js
+++ 17-is-unique/is-unique.js
@@ -1,4 +1,18 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function (arr) {
+    var compareNums = {}
+    for (var i = 0; i < arr.length; i++) {
+        var num = arr[i];
+        if (compareNums[num] === true) {
+            // console.log(false);
+            return false;
+        }
+        compareNums[num] = true;
+    }
+    // console.log(true);
+    return true;
+}
+
+isUnique([20, -15, 18, 2, 3, 60])
