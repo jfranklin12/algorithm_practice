9eb74cbd20d118ba88bdca0c358e8e56e16b34e9
jfranklin12 <j.franks725@gmail.com>
2023-01-24 19:24:09 -0800

    camel case solved

--- 15-camel-case/camel-case.js
+++ 15-camel-case/camel-case.js
@@ -1,6 +1,18 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
 var camelCase = function(str) {
+    var camel = [];
+    var newStr = str.split(" ");
+    // console.log(newStr)
+    for(var i = 1; i < newStr.length; i++){
+        var letters = newStr[i].split('');
+        letters[0] = letters[0].toUpperCase();
+        // console.log(letters[0]);
+        camel.push(letters.join(""));
+    }
+
+    // console.log(newStr[0].toLowerCase() + camel.join(""))
+    return newStr[0].toLowerCase() + camel.join("")
 };
 
-camelCase("let the force flow through you")
+camelCase("The rain in spain falls mainly on the plain")
