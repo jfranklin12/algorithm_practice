7203d575fab440c239c92f3bc8522df856d67586
jfranklin12 <j.franks725@gmail.com>
2023-01-24 19:01:26 -0800

    product of largest two solved

--- 14-product-of-the-largest-two/product-of-largest-two.js
+++ 14-product-of-the-largest-two/product-of-largest-two.js
@@ -1,3 +1,18 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j + 1] > arr[j]) {
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
+            }
+        }
+    }
+    // console.log(arr);
+    var product = 0
+    product = arr[0] * arr[1];
+    // console.log(product)
+    return product;
+};
+
+productOfLargestTwo([10, 20, 9, 60, 3])
